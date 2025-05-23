4f7921ea50983e88db5f4485ade43557d1d6bf1a
Basma <basmaelmenshawy3@gmail.com>
2025-05-23 22:23:54 +0200

    ChallangeProkect

---
+++ ChallengeProject/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] studentsNames = { "John", "Jane", "Jim", "Jill" };
+double[] studentsGrades = { 95.6, 91.6, 89.2, 93.0 };
+string letterGrade = "A";
+for (int i = 0; i < studentsNames.Length; i++)
+{
+    if (studentsGrades[i] > 95)
+    {
+        letterGrade = "A+";
+    }
+    else if (studentsGrades[i] >= 90 && studentsGrades[i] <= 95)
+    {
+        letterGrade = "A";
+    }
+    else if (studentsGrades[i] >= 80 && studentsGrades[i] < 90)
+    {
+        letterGrade = "B";
+    }
+    else if (studentsGrades[i] >= 70 && studentsGrades[i] < 80)
+    {
+        letterGrade = "C";
+    }
+    else if (studentsGrades[i] >= 60 && studentsGrades[i] < 70)
+    {
+        letterGrade = "D";
+    }
+    else
+    {
+        letterGrade = "F";
+    }
+    Console.WriteLine($"Student            Grade                 Letter Grade");
+    Console.WriteLine($"{studentsNames[i]} {studentsGrades[i]}   {letterGrade}");
+
+}
--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -93,7 +93,7 @@ using System.Diagnostics.Metrics;
 
 
 
-List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 // IEnumerable<int> numbersGreaterThanFive = from number in numbers
 //                                            where number > 5
 //                                            orderby number descending
@@ -109,10 +109,68 @@ List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 //                                            where number > 5
 //                                            orderby number descending
 //                                            select $"{number} is greater than 5";
-var numquery= numbers.Where(n => n > 5).OrderByDescending(n => n);
-List<int> mynum = numquery.ToList();
 
-foreach (int number in mynum)
+
+
+// var numquery= numbers.Where(n => n > 5).OrderByDescending(n => n);
+// List<int> mynum = numquery.ToList();
+
+// foreach (int number in mynum)
+// {
+//     Console.WriteLine(number);
+// }   
+
+
+
+
+
+ decimal gradePointAverage = 3.99872831m;
+ Console.WriteLine((int)gradePointAverage); // 3
+
+
+
+
+
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine(number);
-}   
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
